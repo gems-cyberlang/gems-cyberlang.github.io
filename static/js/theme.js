010ecbc05cd430124c8ba88dfee2a7bfc9b991bd
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-05-28 17:10:37 -0400

    Make theme select less ugly

--- static/js/theme.js
+++ static/js/theme.js
@@ -3,23 +3,53 @@ const themeKey = "theme";
 const lightTheme = "theme-light";
 const darkTheme = "theme-dark";
 
-function getTheme() {
-  const stored = localStorage.getItem(themeKey);
-  if (stored !== null) {
-    return stored;
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+function getStoredTheme() {
+  return localStorage.getItem(themeKey);
+}
+
+function getSystemTheme() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     return darkTheme;
   } else {
     return lightTheme;
   }
 }
 
-function setTheme(theme) {
-  localStorage.setItem(themeKey, theme);
-  document.body.className = theme;
+function getTheme() {
+  const stored = localStorage.getItem(themeKey);
+  if (stored !== null) {
+    return stored;
+  } else {
+    return getSystemTheme();
+  }
 }
 
-setTheme(getTheme())
+function setTheme(theme) {
+  if (theme === null) {
+    document.body.className = getSystemTheme();
+    localStorage.removeItem(themeKey);
+  } else {
+    document.body.className = theme;
+    localStorage.setItem(themeKey, theme);
+  }
+
+  const optionElems = document.getElementsByClassName("theme-option");
+  for (let i = 0; i < optionElems.length; i++) {
+    const elem = optionElems.item(i);
+    const themeAttr = elem.attributes.getNamedItem("theme");
+    if (themeAttr === null && theme === null) {
+      elem.classList.add("selected");
+    } else if (themeAttr !== null && themeAttr.value === theme) {
+      elem.classList.add("selected");
+    } else {
+      elem.classList.remove("selected");
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTheme(getStoredTheme());
+})
 
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener(
   "change",
