48e9e6e0021c2f0a569599cf788681865e0493e0
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-08-18 14:13:32 -0400

    Port some css stuff

--- astro/src/consts.ts
+++ astro/src/consts.ts
@@ -1,8 +1,32 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "CYB3RL4NG";
+export const SITE_DESCRIPTION = "Website for Team CYB3RL4NG";
 
-export const CONTACT_NAME = "Foo bar";
+export const CONTACT_NAME = "Team Liaison";
 export const CONTACT_EMAIL = "someone@example.com";
+
+export const PAGES = [
+  ["/about", "About", "AB0UT"],
+  ["/timeline", "Timeline", "T1M3L1N3"],
+  ["/blog", "Blog", "B10G"],
+]
+
+export const SOCIAL_MEDIA_ICONS = [
+  {
+    faClass: "fab fa-github",
+    link: "https://github.com/gems-cyberlang",
+    title: "CYB3RL4NG's GitHub organization"
+  },
+  {
+    faClass: "fab fa-instagram",
+    link: "https://instagram.com/cyb3rl4ng",
+    title: "CYB3RL4NG's Instagram"
+  },
+  {
+    faClass: "fa-solid fa-rss",
+    link: "/index.xml",
+    title: "RSS feed"
+  }
+]
